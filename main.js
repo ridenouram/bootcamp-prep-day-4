5d6a58e0e317f0780fb460eed26992a28181598e
Anna Ridenour <annamridenour@gmail.com>
2019-01-17 19:45:48 -0800

    making greeting!

--- main.js
+++ main.js
@@ -2,13 +2,17 @@
 function greet() {
     
     // get the inputs by id
-
-
+    var firstNameInput = document.getElementById('first-name');
+    var lastNameInput = document.getElementById('last-name');
+    
     // get the values out of inputs
-
+    var firstName = firstNameInput.value;
+    var lastName = lastNameInput.value;
+    
 
     // make the greeting
-
+    var greeting = 'Hello ' + firstName + ' ' + lastName + '!';
+    console.log(greeting);
 
     //display greeting
 
